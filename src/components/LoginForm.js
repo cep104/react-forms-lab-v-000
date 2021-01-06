3b13c018eb28ff5f9782f42ce1bddcf8ca1419fd
cep104 <candicelizabethpeters@gmail.com>
2021-01-06 16:32:32 -0600

    commit tree 4e737ffc8c7bbdb8357109a243b83c03b83c30b2

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,64 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
+  handleChange = (event) => {
+    this.setState(
+      {
+        //event.target.name would be username or password.
+        //name is from the form.
+        //event.target.value is what is being filled in.
+        //if you want to check this use console.log
+        [event.target.name]: event.target.value,
+      },
+      () => console.log(this.state)
+    );
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(this.state);
+      // could also use:
+      // username: this.state.username,
+      //password: this.state.password,
+    }
+  };
 
   render() {
     return (
-      <form>
+      //could also just do onSubmit={this.handleSubmit}
+      <form onSubmit={(event) => this.handleSubmit(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              //onChange={this.handleChange}
+              onChange={(event) => this.handleChange(event)}
+              //make sure value is added
+              value={this.state.username}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              //onChange={this.handleChange}
+              onChange={(event) => this.handleChange(event)}
+              //make sure value is added
+              value={this.state.password}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,38 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      //set the message.
+    };
   }
+  onChange = (event) => {
+    //look at previous lesson for this
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () => console.log(this.state)
+      //use console.log so you can see what is happening while it is happening.
+    );
+  };
 
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
+    //take the maxChars which is passed down and minus it from the message length.
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          onChange={(event) => this.onChange(event)}
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          //make sure to add the value once updated.
+        />
+        {charNumber}
+        {/* //show the charNumbers */}
       </div>
     );
   }
